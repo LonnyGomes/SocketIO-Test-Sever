faa61b974fcf33c641d30b7e0135a932b5a93bd4
Lonny Gomes <code@lonnygomes.com>
2015-03-07 23:22:34 -0500

    Added 404 messages and other boilerplate code from express scaffolding

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname +'/views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // logs every request
 app.use(function(req, res, next){
     // output every request in the array
@@ -35,6 +38,38 @@ app.get("/", function(req, res){
 });
 
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// socket io code
 io.sockets.on('connection', function (socket) {
     io.sockets.emit('blast', {
         timeStamp: Date.now(),
