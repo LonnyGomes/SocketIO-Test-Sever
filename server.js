3f6bb29f2a42da7fc6df314753ab40ed79820c9f
Lonny Gomes <code@lonnygomes.com>
2015-03-07 23:10:03 -0500

    Updated to Express 4.x and SocketIO 1.x

--- server.js
+++ server.js
@@ -7,36 +7,31 @@
 // express magic
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server);
-var device  = require('express-device');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
 var runningPortNumber = process.env.PORT || 7777;
 
 
-app.configure(function(){
-	// I need to access everything in '/public' directly
-	app.use(express.static(__dirname + '/public'));
+// I need to access everything in '/public' directly
+app.use(express.static(__dirname + '/public'));
 
-	//set the view engine
-	app.set('view engine', 'ejs');
-	app.set('views', __dirname +'/views');
-
-	app.use(device.capture());
-});
-
+//set the view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname +'/views');
 
 // logs every request
 app.use(function(req, res, next){
-	// output every request in the array
-	console.log({method:req.method, url: req.url, device: req.device});
+    // output every request in the array
+    console.log({method:req.method, url: req.url, device: req.device});
 
-	// goes onto the next function in line
-	next();
+    // goes onto the next function in line
+    next();
 });
 
 app.get("/", function(req, res){
-	res.render('index', {});
+    res.render('index', {});
 });
 
 
